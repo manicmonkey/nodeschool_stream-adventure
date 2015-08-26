ccca8410641ad2f95ab389f61f23eedc234bbb8e
James Baxter <j.w.baxter@gmail.com>
2015-08-26 22:14:46 +0100

    Completed lesson 4

---
+++ lesson04/program.js
@@ -0,0 +1,7 @@
+var through = require('through2');
+function write(buffer, encoding, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+};
+var stream = through(write);
+process.stdin.pipe(stream).pipe(process.stdout);
